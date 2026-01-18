8362c47ab8f3da9b7e4c20b16d3fac97aa8078ec
MD Waliullah <waliullah.godev@gmail.com>
2026-01-18 14:14:50 +0600

    two-dimentional

---
+++ two-dimentional.js
@@ -0,0 +1,11 @@
+const numbers = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+for (let i = 0; i < numbers.length; i++) {
+  for (let j = 0; j < numbers[i].length; j++) {
+    console.log(numbers[i][j]);
+  }
+}
