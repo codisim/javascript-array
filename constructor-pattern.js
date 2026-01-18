d58edb271b1fb7f2632e87fb8e4f8f9361019388
MD Waliullah <waliullah.godev@gmail.com>
2026-01-18 11:47:13 +0600

    first commit

---
+++ constructor-pattern.js
@@ -0,0 +1,16 @@
+const names = new Array();
+
+console.log(names, names.length);
+
+
+
+// factory
+const numbers = Array();
+
+console.log(numbers, numbers.length);
+
+
+
+
+console.log(names.__proto__.constructor);
+console.log(numbers.__proto__.constructor);
