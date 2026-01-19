85e96a6d9cd16112b46ef9d45e321cb3358c2f40
MD Waliullah <waliullah.godev@gmail.com>
2026-01-19 20:36:54 +0600

    feat: find and list implement

--- cli-project-idea/src/index.js
+++ cli-project-idea/src/index.js
@@ -3,7 +3,6 @@ const path = require("path");
 const TODO = require("./Todo");
 const { ADD, UPDATE, DONE, NEXT, LIST, FIND } = require("./cmd");
 const { saveFile, readFile } = require("./utils");
-const { Network } = require("inspector/promises");
 
 const fileName = "../data.json";
 const filePath = path.resolve(__dirname, fileName);
@@ -32,6 +31,37 @@ const filePath = path.resolve(__dirname, fileName);
       const item = todo.next();
       console.log(`${item.id} - ${item.text} - ${item.created}`);
       break;
+    }
+
+    case DONE: {
+      const item = todo.done();
+      console.log(`${item.id} - ${item.text} - ${item.created}`);
+      saveFile(todo.list(), filePath);
+      break;
+    }
+
+    case FIND: {
+        const items = todo.find(argv.term);
+        if (items.length === 0) {
+            console.log("No Item found");
+            break;
+        }
+        for (let i = 0; i < items.length; i++) {
+            console.log(`${items[i].id} - ${items[i].text} - ${items[i].created}`);
+        }
+        break;
+    }
+
+     case LIST: {
+        const items = todo.find(argv.term);
+        if (items.length === 0) {
+            console.log("Empty List");
+            break;
+        }
+        for (let i = 0; i < items.length; i++) {
+            console.log(`${items[i].id} - ${items[i].text} - ${items[i].created}`);
+        }
+        break;
     }
 
     default:
