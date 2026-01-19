6ba736733921f310ce2b05a4da2071dffa22882b
MD Waliullah <waliullah.godev@gmail.com>
2026-01-19 15:12:01 +0600

    feat: readFile util func

--- cli-project-idea/src/utils.js
+++ cli-project-idea/src/utils.js
@@ -8,3 +8,14 @@ module.exports.saveFile = (data, fileName) => {
     throw new Error(error);
   }
 };
+
+
+
+module.exports.readFile = (fileName) => {
+  try {
+    fs.readFileSync(fileName, "utf-8");
+  } catch (error) {
+    console.log(error.message);
+    throw new Error(error);
+  }
+};
