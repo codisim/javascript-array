fbeef657a41d26dfa4a50e29d8a20db19c2c311c
MD Waliullah <waliullah.godev@gmail.com>
2026-01-20 15:06:46 +0600

    custom compose func

--- compose-func.js
+++ compose-func.js
@@ -4,6 +4,25 @@ const add10 = (a) => a + 10;
 const times3 = (a) => a * 3;
 const div2 = (a) => a / 2;
 
-
-const x = div2(times3(add10(5)));
+const n = 5;
+const x = div2(times3(add10(n)));
 console.log(x);
+
+
+// pipe func - short way
+const pipe = (...fns) => x => fns.reduce((v, f) => f(v), n);
+const r1 = pipe(add10, times3, div2)(n);
+
+console.log(r1);
+
+
+// compose func - long way
+const compose = (...fns) => {
+    return (x) => {
+        return fns.reduce((v, f) => f(v), x);
+    }
+}
+
+const r2 = compose(add10, times3, div2)(n);
+
+console.log(r2);
