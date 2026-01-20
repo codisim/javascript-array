9fe07217455eeca66cf27842032959880efa787c
MD Waliullah <waliullah.godev@gmail.com>
2026-01-20 14:22:03 +0600

    reduce implement into product

--- reduce.js
+++ reduce.js
@@ -28,4 +28,54 @@ const result = votes.reduce((acc, curr) => {
     return acc;
 }, {})
 
-console.log(result);
+// console.log(result);
+
+
+
+
+const products = [
+    {name: 'product1', price: 10},
+    {name: 'product2', price: 20},
+    {name: 'product2', price: 20},
+    {name: 'product3', price: 30},
+    {name: 'product3', price: 30},
+    {name: 'product3', price: 30},
+    {name: 'product4', price: 40},
+    {name: 'product4', price: 40},
+    {name: 'product4', price: 40},
+    {name: 'product4', price: 40},
+    {name: 'product5', price: 50},
+    {name: 'product5', price: 50},
+    {name: 'product6', price: 60},
+    {name: 'product6', price: 60},
+    {name: 'product6', price: 60},
+    {name: 'product6', price: 60},
+    {name: 'product6', price: 60},
+    {name: 'product6', price: 60},
+]
+
+
+const invoice = products.reduce((acc, curr) => {
+    if (acc[curr.name]) {
+        acc[curr.name].quantity++;
+        acc[curr.name].price += curr.price;
+    } else {
+        acc[curr.name] = {
+            price: curr.price,
+            quantity: 1,
+        };    
+    }
+    return acc;
+}, {});
+
+
+
+console.log(invoice);
+
+
+
+
+
+
+
+
