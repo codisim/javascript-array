d42483a35c07c96ca2ceb222fb074aea586d2d0b
MD Waliullah <waliullah.godev@gmail.com>
2026-01-21 11:28:07 +0600

    array obj

---
+++ arr-obj.js
@@ -0,0 +1,13 @@
+
+
+const numbers = [3, 1, 6, 3];
+
+console.log(typeof numbers);
+console.log(numbers.constructor);
+console.log(numbers.constructor.prototype);
+
+const obj = {};
+
+console.log(numbers instanceof Array);
+console.log(obj instanceof Array);
+
