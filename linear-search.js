4c23449748e52198c11c49393eb917c6d35bbebc
MD Waliullah <waliullah.godev@gmail.com>
2026-01-21 07:53:52 +0600

    linear

---
+++ linear-search.js
@@ -0,0 +1,17 @@
+const numbers = [
+  10, 45, 2, 54, 4, 987, 85, 23, 65, 87, 90, 78, 23, 33, 1, 34, 54, 65, 76, 87, 98,
+  100,
+];
+
+
+const LinearSearch = (arr, key) => {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === key) {
+            return i;
+        }
+    }
+    return - 1;
+}
+
+const s1 = LinearSearch(numbers, 90);
+console.log(s1);
