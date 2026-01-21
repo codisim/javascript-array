e088efdfea264a23609555569a380ffdf1478f17
MD Waliullah <waliullah.godev@gmail.com>
2026-01-21 11:16:45 +0600

    for of

--- iterator.js
+++ iterator.js
@@ -8,3 +8,24 @@ console.log(iArr.next());
 console.log(iArr.next());
 console.log(iArr.next());
 console.log(iArr.next());
+
+
+const keys = arr.keys();
+const values = arr.values();
+const entries = arr.entries();
+
+
+const loop = (it, name) => {
+    for(let v of it){
+        console.log(`${name} - ${v}`);
+    }
+}
+
+
+loop(keys, 'keys');
+loop(values, 'values');
+loop(entries, 'entries');
+
+
+// for-of loop kaj kore only for iterator.
+// Arrar te kaj kore karon Arrar ak prokar iterator
