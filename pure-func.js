09ec6cd28f7296f5cd85c9ec6f446554abe63d34
MD Waliullah <waliullah.godev@gmail.com>
2026-01-28 07:51:32 +0600

    modifies original array

--- pure-func.js
+++ pure-func.js
@@ -22,3 +22,14 @@ incrementCounter(); // counter is now 2
 function pureIncrementCounter(count) {
     return count + 1; // does not modify external state
 }
+
+
+const arr = [1, 2, 3, 4, 5];
+const nums = [1, 2, 3, 4, 5];
+function add(arr, data){ // this is impure function because it modifies original array
+    arr.push(data);
+}
+
+const newArr = add(nums, 6); // modifies original array
+
+console.log(newArr);
